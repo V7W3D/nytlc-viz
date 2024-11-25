d090862181a8a221983170b43e443567faf96a55
Ahmed <sidiahmedahmed2001@gmail.com>
2024-11-25 18:39:54 +0100

    final-version

--- App/app.py
+++ App/app.py
@@ -5,7 +5,8 @@ import geopandas as gpd
 import pandas as pd
 from datetime import date
 
-taxi_zones = gpd.read_file('../Data/taxi_zones_final/taxi_zones_final.shp')
+zones = pd.read_csv("../Data/taxi_zone_lookup.csv")
+taxi_zones = gpd.read_file('../Data/taxi_zones_final.shp')
 yellow_pickup_counts = pd.read_parquet('../Data/yellow_pickup_counts.parquet')
 yellow_dropoff_counts = pd.read_parquet('../Data/yellow_dropoff_counts.parquet')
 green_pickup_counts = pd.read_parquet('../Data/green_pickup_counts.parquet')
@@ -16,7 +17,9 @@ yellow_dropoff_counts['date'] = pd.to_datetime(yellow_dropoff_counts['date'])
 green_pickup_counts['date'] = pd.to_datetime(green_pickup_counts['date'])
 green_dropoff_counts['date'] = pd.to_datetime(green_dropoff_counts['date'])
 
-taxi_type = st.selectbox("Choisissez le type de taxi :", ["Jaune", "Vert"])
+yellow_zones_ids = zones[zones['Borough'] == 'Manhattan']['LocationID'].values
+
+option = st.selectbox("Choisissez l'analyse :", ["Taxis Jaunes", "Taxis Verts", "Empiètements des taxis verts"])
 view_type = st.radio("Choisissez la vue :", ["Pickup", "Dropoff"])
 
 selected_date = st.date_input(
@@ -25,28 +28,49 @@ selected_date = st.date_input(
     max_value=max(yellow_pickup_counts['date'].max(), green_pickup_counts['date'].max())
 )
 
-if taxi_type == "Jaune":
+if option == "Taxis Jaunes":
     data_source = yellow_pickup_counts if view_type == "Pickup" else yellow_dropoff_counts
-elif taxi_type == "Vert":
+elif option == "Taxis Verts":
     data_source = green_pickup_counts if view_type == "Pickup" else green_dropoff_counts
+elif option == "Empiètements des taxis verts":
+    data_source = green_pickup_counts[
+        (green_pickup_counts['date'] == pd.Timestamp(selected_date)) & 
+        (green_pickup_counts['LocationID'].isin(yellow_zones_ids))
+    ]
 
 filtered_data = data_source[data_source['date'] == pd.Timestamp(selected_date)]
 
 m = folium.Map([40.7, -74], zoom_start=10, tiles="cartodbpositron")
 
+fill_color = "Greens" if option == "Empiètements des taxis verts" else "YlOrRd" if option == "Taxis Jaunes" else "GnBu"
+
 folium.Choropleth(
     geo_data=taxi_zones,
-    name="choropleth",
+    name=option,
     data=filtered_data,
     columns=["LocationID", "count"],
     key_on="feature.properties.OBJECTID",
-    nan_fill_color="yellow",
-    nan_fill_opacity=0.4,
-    fill_color="YlOrRd",
+    fill_color=fill_color,
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name=f"Nombre de {view_type}s"
+    nan_fill_color="transparent",
+    legend_name=f"Nombre de {view_type.lower()}s" if option != "Empiètements des taxis verts" else "Empiètements des taxis verts"
 ).add_to(m)
 
-st.title(f"Carte interactive des {view_type.lower()}s de taxi {taxi_type.lower()} à New York")
+folium.features.GeoJson(
+    taxi_zones,
+    name="Zones",
+    tooltip=folium.features.GeoJsonTooltip(
+        fields=["Zone", "OBJECTID"],
+        aliases=["Zone :", "ID de la zone :"],
+        localize=True
+    ),
+    style_function=lambda x: {
+        'fillOpacity': 0,
+        'color': 'transparent',
+        'weight': 0
+    }
+).add_to(m)
+
+st.title(f"Carte interactive - {option}")
 st_folium(m, width=700, height=500)
